abb290e2646c3157d8a4973e77c478718d0c6ce0
Lenusik <tatarinova.elena@gmail.com>
2015-06-07 01:02:12 +0300

    Добавлены параметры для запуска из командной строки и генерация случайных тестовых данных

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = webdriver.Firefox()
-        self.base_url = "http://localhost/"
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome("b:\QA\chromedriver.exe")
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         driver = self.driver
-        driver.get(self.base_url + "addressbook/")
+        driver.get(self.base_url)
 
     def destroy(self):
         self.driver.quit()
